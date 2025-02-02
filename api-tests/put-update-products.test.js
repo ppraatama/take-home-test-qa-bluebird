d9b9a94f57788d9bd00afe8033bcc1e9776fb30f
admin <ppraatama@gmail.com>
2025-02-02 10:59:52 +0700

    Add: Add some edge case in get, post, put, and patch products

--- api-tests/put-update-products.test.js
+++ api-tests/put-update-products.test.js
@@ -6,12 +6,20 @@ let categoryId = "";
 let imageId;
 let productId = "";
 
+function generateRandomString(length) {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+}
+
 beforeEach(async () => {
-  const getProducts = await request(baseURL)
-    .get(`/products`)
-    .send({
-      page: 1
-    });
+  const getProducts = await request(baseURL).get(`/products`).send({
+    page: 1,
+  });
 
   console.log("Response Body:", getProducts.body);
 
@@ -30,7 +38,7 @@ beforeEach(async () => {
   }
 });
 
-describe("Update Products By Id", () => {
+describe("Update products in practice software testing with method PUT", () => {
   test("Scenario: Verify success update product id", async () => {
     const response = await request(baseURL)
       .put(`/products/${productId}`)
@@ -47,11 +55,77 @@ describe("Update Products By Id", () => {
       .set("Content-Type", "application/json");
 
     console.log("Response Body:", response.body);
-    console.log("Response Status:", response.status)
+    console.log("Response Status:", response.status);
 
     expect(response.status).toBe(200);
-    console.log(response.status)
+    console.log(response.status);
     expect(response.body).toHaveProperty("success");
-    console.log(response.body)
+    console.log(response.body);
+  });
+
+  test("Scenario: Verify user input wrong method", async () => {
+    const response = await request(baseURL)
+      .post(`/products/${productId}`)
+      .send({
+        name: "Updated Produk-Test",
+        description: "Updated Deskripsi Produk",
+        price: 1.99,
+        category_id: categoryId,
+        brand_id: brandId,
+        product_image_id: imageId,
+        is_location_offer: 1,
+        is_rental: 0,
+      })
+      .set("Content-Type", "application/json");
+
+    console.log("Response Body:", response.body);
+    console.log("Response Status:", response.status);
+
+    console.log(response.status);
+    console.log(response.body);
+    expect(response.status).toBe(405);
+    expect(response.body).toHaveProperty("message");
+  });
+
+  test("Scenario: Verify When Returns when the resource is not found", async () => {
+    const response = await request(baseURL)
+      .put(`/product/${productId}`)
+      .send({
+        name: "Updated Produk-Test",
+        description: "Updated Deskripsi Produk",
+        price: 1.99,
+        category_id: categoryId,
+        brand_id: brandId,
+        product_image_id: imageId,
+        is_location_offer: 1,
+        is_rental: 0,
+      })
+      .set("Content-Type", "application/json");
+
+    console.log("Response Body:", response.body);
+    console.log("Response Status:", response.status);
+
+    expect(response.status).toBe(404);
+    expect(response.body).toHaveProperty("message");
+  });
+
+  test("Scenario: Verify return status code 422 when the server was not able to process", async () => {
+    const response = await request(baseURL)
+      .put(`/products/${productId}`)
+      .send({
+        name: `${generateRandomString(5)}`,
+        description: `${generateRandomString(5)}`,
+        price: 1.99,
+        category_id: categoryId,
+        brand_id: brandId,
+        product_image_id: 1,
+        is_location_offer: 4,
+        is_rental: 1,
+      })
+      .set("Content-Type", "application/json");
+
+    console.log("Response Body:", response.body);
+    console.log("Response Status:", response.status);
+    expect(response.status).toBe(422);
   });
 });
